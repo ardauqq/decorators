a54ae04e087c1001f403b84b8d00fa3affef3f22
Viktor Batyrov <derbety2001@yandex.ru>
2023-08-03 10:54:24 +0300

    Initial commit

---
+++ decor_3.py
@@ -0,0 +1,33 @@
+import types
+from decor_2 import logger
+
+
+@logger('decor_3.log')
+def flat_generator(list_of_lists):
+
+    for listes in list_of_lists:
+        for item in listes:
+            yield item
+
+
+def test_3():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_3()
